1826d39a32a719caf15ce23d7794d6efb0487c12
dewdhiibrahim <wwadah38@gmail.com>
2021-09-10 19:05:30 +0200

    frist test and import junit

--- AvansertJava-innlevering/src/main/java/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/ToRomanConverter.java
@@ -1,2 +1,5 @@
 public class ToRomanConverter {
+    public static String getRoman(int i) {
+        return "I";
+    }
 }
--- AvansertJava-innlevering/src/test/java/ToRomanConverterTest.java
+++ AvansertJava-innlevering/src/test/java/ToRomanConverterTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ToRomanConverterTest {
+ @Test
+ void test1(){
+
+     assertEquals("I",ToRomanConverter.getRoman(1));
+
+ }
 }
