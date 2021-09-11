abea079ef94294ba7ab2a7d55b35f4979cc5d81d
dewdhiibrahim <wwadah38@gmail.com>
2021-09-11 14:13:29 +0200

    convert number 6

--- AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
@@ -8,13 +8,13 @@ public class ToRomanConverter {
 
         if (romanResult >= 4){
             output += "IV";
+            if  (romanResult >= 6){
+                output +="VI";
+                romanResult = romanResult -1;
+            }
             romanResult = romanResult -1;
         } else if (romanResult >= 1 ){
             output += "I";
-            romanResult = romanResult -1;
-        }
-        if  (romanResult >= 6){
-            output +="VI";
             romanResult = romanResult -1;
         }
 
