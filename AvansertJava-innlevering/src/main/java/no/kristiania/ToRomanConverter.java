4549634686584b711a2aa3738e1a758a996579cd
dewdhiibrahim <wwadah38@gmail.com>
2021-09-11 16:31:41 +0200

    fixe the code

--- AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
@@ -2,28 +2,25 @@ package no.kristiania;
 
 public class ToRomanConverter {
 
-    public static String ToRoman(int number) {
+    public static String  ToRoman(int number) {
         int romanResult = number;
         String output = "";
-        if  (romanResult >= 9){
-            output +="IX";
-            romanResult = romanResult -1;
+        if (romanResult >= 9) {
+            output += "IX";
+            romanResult = romanResult - 1;
 
-        if  (romanResult >= 6){
-            output +="VI";
-            romanResult = romanResult -1;
+        } else if (romanResult >= 6) {
+                output += "VI";
+                romanResult = romanResult - 1;
+            } else if (romanResult >= 4) {
+                output += "IV";
+
+                romanResult = romanResult - 1;
+            } else if (romanResult >= 1) {
+                output += "I";
+                romanResult = romanResult - 1;
+            }
+
+            return (output);
         }
-
-        else if (romanResult >= 4){
-            output += "IV";
-
-            romanResult = romanResult -1;
-        } else if (romanResult >= 1 ){
-            output += "I";
-            romanResult = romanResult -1;
-        }
-
-
-     return output;
     }
-}
