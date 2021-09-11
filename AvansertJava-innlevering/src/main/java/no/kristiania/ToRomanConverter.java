dbc7887798eb29f16246c95caa2a1dbf30f08fcf
dewdhiibrahim <wwadah38@gmail.com>
2021-09-11 17:02:00 +0200

    Add 4 romantall arrays

--- AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
@@ -1,8 +1,13 @@
 package no.kristiania;
 
 public class ToRomanConverter {
+    static String[] rn_M = {"", "M", "MM", "MMM"};
+    static String[] rn_C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    static String[] rn_X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static String[] rn_I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
     public static String  ToRoman(int number) {
+
 
         String output = "";
         if (number >= 9) {
@@ -22,5 +27,13 @@ public class ToRomanConverter {
             }
 
             return (output);
+        }
+
+        public static void tesToRoman(int num){
+
+            System.out.println(rn_I[num]);
+
+
+
         }
     }
