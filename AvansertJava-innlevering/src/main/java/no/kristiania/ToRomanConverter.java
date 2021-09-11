74539b174092f10166ca38c977d82e24919d2ab0
dewdhiibrahim <wwadah38@gmail.com>
2021-09-11 17:03:15 +0200

    remove ToRoman method

--- AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
@@ -6,28 +6,6 @@ public class ToRomanConverter {
     static String[] rn_X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
     static String[] rn_I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
-    public static String  ToRoman(int number) {
-
-
-        String output = "";
-        if (number >= 9) {
-            output += "IX";
-
-
-        } else if (number >= 6) {
-                output += "VI";
-
-            } else if (number >= 4) {
-                output += "IV";
-
-
-            } else if (number >= 1) {
-                output += "I";
-
-            }
-
-            return (output);
-        }
 
         public static void tesToRoman(int num){
 
