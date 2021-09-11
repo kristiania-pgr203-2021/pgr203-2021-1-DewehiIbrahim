7deb48d322fa1eecb97e97becee336ea11a96770
fredrik <fredrik.a.holtet@gmail.com>
2021-09-11 19:21:14 +0200

    wrote an scanner for project(Not working)

--- AvansertJava-innlevering/src/main/java/no/kristiania/Main.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/Main.java
@@ -1,7 +1,20 @@
 package no.kristiania;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a number you would like to convert to RomanNumeral");
+        int scanNumber = scanner.nextInt();
+
+        if (scanNumber <= 1 || scanNumber > 3999){
+            System.out.println(" Pliz type in a number between 1 and 3999");
+            scanner.close();
+        } else {
+            ToRomanConverter.tesToRoman(scanNumber);
+            scanner.close();
+        }
         System.out.println(ToRomanConverter.tesToRoman(223));
        // ToRomanConverter.tesToRoman(12);
 
