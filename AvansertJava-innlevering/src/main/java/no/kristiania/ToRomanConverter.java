0cf338e504d9c60342a1121936af6608a8e780a5
dewdhiibrahim <wwadah38@gmail.com>
2021-09-11 14:17:59 +0200

    convert number 6

--- AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
@@ -5,13 +5,14 @@ public class ToRomanConverter {
     public static String ToRoman(int number) {
         int romanResult = number;
         String output = "";
+        if  (romanResult >= 6){
+            output +="VI";
+            romanResult = romanResult -1;
+        }
 
         if (romanResult >= 4){
             output += "IV";
-            if  (romanResult >= 6){
-                output +="VI";
-                romanResult = romanResult -1;
-            }
+
             romanResult = romanResult -1;
         } else if (romanResult >= 1 ){
             output += "I";
