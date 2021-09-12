a37926392670f1d469a59181b2f05ac30b11f491
dewdhiibrahim <wwadah38@gmail.com>
2021-09-13 00:32:46 +0200

    update the code and create  a new method called start

--- AvansertJava-innlevering/src/main/java/no/kristiania/Main.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/Main.java
@@ -1,21 +1,12 @@
 package no.kristiania;
 
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Write a number you would like to convert to RomanNumeral");
-        int scanNumber = scan.nextInt();
 
-        if (scanNumber < 1 || scanNumber > 3999){
-            System.out.println(" type in a number between 1 and 3999");
+         ToRomanConverter.start();
 
-        } else {
-            ToRomanConverter.tesToRoman(scanNumber);
-            scan.close();
-        }
+}
 
-
-    }
 }
--- AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
+++ AvansertJava-innlevering/src/main/java/no/kristiania/ToRomanConverter.java
@@ -1,16 +1,23 @@
 package no.kristiania;
 
+import java.util.Scanner;
+
 public class ToRomanConverter {
     static String[] rn_M = {"", "M", "MM", "MMM"};
     static String[] rn_C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
     static String[] rn_X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
     static String[] rn_I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
+    public static void start() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write a number you would like to convert to RomanNumeral");
+        int scanNumber = scan.nextInt();
+        System.out.println(tesToRoman(scanNumber));
+        scan.close();
+    }
+
 
         public static String tesToRoman(int num) {
-
-           // System.out.println(rn_I[num]);
-            //return rn_I[num];
 
             String output="";
             String total=String.valueOf(num);
@@ -32,8 +39,10 @@ public class ToRomanConverter {
 
             } else if (num <= 3999){
                 output= rn_M[spilt(total,0,1)]+rn_C[spilt(total,1,2)]+rn_X[spilt(total,2,3)]+rn_I[spilt(total,3,4)];
-        }
-            System.out.println(num + " is " + output + " in Roman");
+        }else {
+                System.out.println("The number:"+num+" can not be convert to Roman ");
+            }
+
             return output;
  }
 
